f9f9f7889f81d1c1fb76182294a3902d7c47cd92
André Storhaug <andr3.storhaug@gmail.com>
2017-12-28 01:56:42 +0100

    Linting fixes.

--- public/js/client.js
+++ public/js/client.js
@@ -1,101 +1,101 @@
 function messageHandling() {
 
-    // My name sent to the server
-    var myName = false;
+	// My name sent to the server
+	var myName = false;
 
-    // Whom shall recieve message (KID or "all").
-    var recieverKID = "all";
+	// Whom shall recieve message (KID or "all").
+	var recieverKID = 'all';
 
-    // If user is running mozilla then use it's built-in WebSocket
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
+	// If user is running mozilla then use it's built-in WebSocket
+	window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-    // If browser doesn't support WebSocket, just show some notification and exit
-    if (!window.WebSocket) {
-        content.html($('<p>', {
-            text: 'Sorry, but your browser doesn\'t ' +
+	// If browser doesn't support WebSocket, just show some notification and exit
+	if (!window.WebSocket) {
+		content.html($('<p>', {
+			text: 'Sorry, but your browser doesn\'t ' +
                 'support WebSockets.'
-        }));
-        $('#input').hide();
-        return;
-    }
+		}));
+		$('#input').hide();
+		return;
+	}
 
 
-    var HOST = location.origin.replace(/^http/, 'ws');
-    var ws = new WebSocket(HOST);
-    // Log a message when connection is successful.
+	var HOST = location.origin.replace(/^http/, 'ws');
+	var ws = new WebSocket(HOST);
+	// Log a message when connection is successful.
 
-    ws.onopen = function() {
-        console.log('Connected to server');
-        // first we want users to enter their names
-        $('#input').removeAttr('disabled');
-        $('#input').attr("placeholder", "Enter a name ...");
-    };
+	ws.onopen = function() {
+		console.log('Connected to server');
+		// first we want users to enter their names
+		$('#input').removeAttr('disabled');
+		$('#input').attr('placeholder', 'Enter a name ...');
+	};
 
-    ws.onerror = function(error) {
-        // just in there were some problems with conenction...
-        $('.content').html($('<p>', {
-            text: 'Sorry, but there\'s some problem with your ' +
+	ws.onerror = function(error) {
+		// just in there were some problems with conenction...
+		$('.content').html($('<p>', {
+			text: 'Sorry, but there\'s some problem with your ' +
                 'connection or the server is down.'
-        }));
-    };
+		}));
+	};
 
-    // When a message from server is received, we display it on the screen.
-    ws.onmessage = function(message) {
-        var msgReceived = message.data;
-        console.log("Websocket server responds: " + msgReceived);
+	// When a message from server is received, we display it on the screen.
+	ws.onmessage = function(message) {
+		var msgReceived = message.data;
+		console.log('Websocket server responds: ' + msgReceived);
 
-        // try to parse JSON message. Because we know that the server always returns
-        // JSON this should work without any problem but we should make sure that
-        // the massage is not chunked or otherwise damaged.
-        var json;
+		// try to parse JSON message. Because we know that the server always returns
+		// JSON this should work without any problem but we should make sure that
+		// the massage is not chunked or otherwise damaged.
+		var json;
 
-        try {
-            json = JSON.parse(message.data);
-            console.log(json);
+		try {
+			json = JSON.parse(message.data);
+			console.log(json);
 
-        } catch (e) {
-            console.log('This doesn\'t look like a valid JSON: ', message.data);
-            return;
-        }
-        if (json.type === 'init') {
-            $('#input').removeAttr('disabled').focus();
-            // from now user can start sending messages
-            $('#input').attr('placeholder', 'Message');
+		} catch (e) {
+			console.log('This doesn\'t look like a valid JSON: ', message.data);
+			return;
+		}
+		if (json.type === 'init') {
+			$('#input').removeAttr('disabled').focus();
+			// from now user can start sending messages
+			$('#input').attr('placeholder', 'Message');
 
-            document.getElementById('username').innerHTML = "@" + json.data + " <span class='light_text'>(you)</span>"; // Show the username
+			document.getElementById('username').innerHTML = '@' + json.data + ' <span class=\'light_text\'>(you)</span>'; // Show the username
 
-        } else if (json.type === 'nameList') {
-            console.log(json.data);
-            addClientsNames(json);
+		} else if (json.type === 'nameList') {
+			console.log(json.data);
+			addClientsNames(json);
 
-        } else if (json.type === 'history') { // entire message history
-            // insert every single message to the chat window
-            for (var i = 0; i < json.data.length; i++) {
-                addMessage(json.data[i].author, json.data[i].text, new Date(json.data[i].time));
-            }
+		} else if (json.type === 'history') { // entire message history
+			// insert every single message to the chat window
+			for (var i = 0; i < json.data.length; i++) {
+				addMessage(json.data[i].author, json.data[i].text, new Date(json.data[i].time));
+			}
 
-        } else if (json.type === 'message') { // it's a single message
-            $('#input').removeAttr('disabled'); // let the user write another message
-            addMessage(json.data.author, json.data.text, new Date(json.data.time));
-        } else {
-            console.log('Hmm..., I\'ve never seen JSON like this: ', json);
-        }
-
-
-    };
-
-    // As soon as the connection is closed, we inform the user.
-    ws.onclose = function() {
-        console.log('Disconnected from server');
-    };
+		} else if (json.type === 'message') { // it's a single message
+			$('#input').removeAttr('disabled'); // let the user write another message
+			addMessage(json.data.author, json.data.text, new Date(json.data.time));
+		} else {
+			console.log('Hmm..., I\'ve never seen JSON like this: ', json);
+		}
 
 
+	};
 
-    /**
+	// As soon as the connection is closed, we inform the user.
+	ws.onclose = function() {
+		console.log('Disconnected from server');
+	};
+
+
+
+	/**
      * Send mesage when user presses Enter key
 
      */
-    /*
+	/*
         document.getElementById('send').onclick = function() {
             //            var msgSend = document.getElementById('entry').value;
             //            log("Browser sends: " + msgSend);
@@ -112,17 +112,17 @@ function messageHandling() {
 
 
 
-    $('textarea').keydown(function(e) {
+	$('textarea').keydown(function(e) {
 
-        if (e.keyCode === 13 && !e.shiftKey) {
-            e.preventDefault();
+		if (e.keyCode === 13 && !e.shiftKey) {
+			e.preventDefault();
 
-            var msg = $(this).val();
-            if (!msg) {
-                return;
-            }
+			var msg = $(this).val();
+			if (!msg) {
+				return;
+			}
 
-            /*
+			/*
                     var data = {
                         to:      "sec-websocket-identifier",
                         from:    name,
@@ -134,62 +134,62 @@ function messageHandling() {
                     ws.send(JSON.stringify(data));
             */
 
-            ws.send(msg);
-            $(this).val('');
-            // disable the input field to make the user wait until server
-            // sends back response
-            $('#input').attr('disabled', 'disabled');
+			ws.send(msg);
+			$(this).val('');
+			// disable the input field to make the user wait until server
+			// sends back response
+			$('#input').attr('disabled', 'disabled');
 
-            // we know that the first message sent from a user their name
-            if (myName === false) {
-                myName = msg;
-            }
-        }
-    });
+			// we know that the first message sent from a user their name
+			if (myName === false) {
+				myName = msg;
+			}
+		}
+	});
 
 
-    /**
+	/**
      * This method is optional. If the server wasn't able to respond to the
      * in 3 seconds then show some error message to notify the user that
      * something is wrong.
      */
-    setInterval(function() {
-        if (ws.readyState !== 1) {
-            $('#input').attr('placeholder', 'Error');
-            $('#input').attr('disabled', 'disabled').val('Unable to comminucate ' +
+	setInterval(function() {
+		if (ws.readyState !== 1) {
+			$('#input').attr('placeholder', 'Error');
+			$('#input').attr('disabled', 'disabled').val('Unable to comminucate ' +
                 'with the WebSocket server.');
-        }
-    }, 3000);
+		}
+	}, 3000);
 
-    /**
+	/**
      * Add message to the chat window
      */
-    function addMessage(author, message, dt) {
-        $('#msgs_board').append('<div class="message_content">' + '<b class="author">' + author + '</b>' + ' ' + '<div class="timestamp">' + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':' +
+	function addMessage(author, message, dt) {
+		$('#msgs_board').append('<div class="message_content">' + '<b class="author">' + author + '</b>' + ' ' + '<div class="timestamp">' + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':' +
             (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes()) + '</div>' + '<br/> ' + message + '</div>');
-        updateScroll();
-    }
+		updateScroll();
+	}
 
 
-    function addClientsNames(json) {
-        $('#clients-list').html("");
-        console.log("lol");
-        for (var j = 0; j < json.data.length; j++) {
-            $('#clients-list').append('<li>' + json.data[j].name + '</li>');
+	function addClientsNames(json) {
+		$('#clients-list').html('');
+		console.log('lol');
+		for (var j = 0; j < json.data.length; j++) {
+			$('#clients-list').append('<li>' + json.data[j].name + '</li>');
 
-            if (json.data[j].KID === recieverKID) {
-                document.getElementById('clients-list').getElementsByTagName("li")[j].className = "active";
-                console.log("Virker");
-            } else {
-                console.log("virker ikke");
-            }
-        }
-    }
+			if (json.data[j].KID === recieverKID) {
+				document.getElementById('clients-list').getElementsByTagName('li')[j].className = 'active';
+				console.log('Virker');
+			} else {
+				console.log('virker ikke');
+			}
+		}
+	}
 
-    function updateScroll() {
-        var element = document.getElementById("msgs_board");
-        element.scrollTop = element.scrollHeight;
-    }
+	function updateScroll() {
+		var element = document.getElementById('msgs_board');
+		element.scrollTop = element.scrollHeight;
+	}
 
 
 
@@ -198,6 +198,6 @@ function messageHandling() {
 
 //Runs when the document is loaded. Initialises the script.
 document.addEventListener('DOMContentLoaded', function() {
-    "use strict";
-    messageHandling();
+	'use strict';
+	messageHandling();
 });
